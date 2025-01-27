b02c4c514f517237aeacdc10b11189923e0a458b
Yusaku Taki <yusaku.taki@tw.tokai-tv.co.jp>
2025-01-27 11:32:34 +0900

    envにアカウント設定を一応追加

--- setup-env.sh
+++ setup-env.sh
@@ -35,3 +35,7 @@ export VITE_APP_SAML_COGNITO_FEDERATED_IDENTITY_PROVIDER_NAME=`extract_value "$s
 export VITE_APP_AGENT_NAMES=`extract_value "$stack_output" AgentNames`
 export VITE_APP_USE_CASE_BUILDER_ENABLED=`extract_value "$stack_output" UseCaseBuilderEnabled`
 export VITE_APP_OPTIMIZE_PROMPT_FUNCTION_ARN=`extract_value "$stack_output" OptimizePromptFunctionArn`
+
+# AWS account指定
+export AWS_PROFILE='digi-poc'
+export AWS_DEFAULT_REGION='us-east-1'
